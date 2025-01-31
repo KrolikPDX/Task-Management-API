89905121ade30e6262b80ffd43647d5b9537a8d5
Joseph Demyanovskiy <48137744+KrolikPDX@users.noreply.github.com>
2025-01-30 19:29:45 -0800

    Added react UI portion to project

--- api/src/tasks/controllers/task.controller.ts
+++ api/src/tasks/controllers/task.controller.ts
@@ -1,19 +1,13 @@
-import { Controller, Get, Post, Body, NotFoundException, BadRequestException, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, NotFoundException, BadRequestException, Param, Put, Delete } from '@nestjs/common';
 import { TaskService } from '../services/task.service';
 import { TaskModel } from '../models/task.interface';
 import { map, Observable } from 'rxjs';
 import { get } from 'http';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
 @Controller('task')
 export class TaskController {
     constructor(private taskService: TaskService) {}
-
-    @Post() //Creates new task and adds it to DB
-    create(@Body() task: TaskModel): Observable<TaskModel> { //From the body of the request, create a task given the object
-        if (!task.title) throw new BadRequestException('Title is a required field.');
-        if (!task.description) throw new BadRequestException('Description is a required field.');
-        return this.taskService.createTask(task)
-    }
 
     @Get() //Returns list of all tasks
     getAllTasks(): Observable<TaskModel[]> {
@@ -25,10 +19,27 @@ export class TaskController {
         return this.taskService.getTaskById(id).pipe(
             map((task) => {
                 if (!task) {
-                throw new NotFoundException(`Task with ID '${id}' not found`);
+                    throw new NotFoundException(`Task with ID '${id}' not found`);
                 }
                 return task; 
             }),
         );
+    }
+
+    @Post() //Creates new task and adds it to DB
+    createTask(@Body() task: TaskModel): Observable<TaskModel> { //From the body of the request, create a task given the object
+        if (!task.title) throw new BadRequestException('Title is a required field.');
+        if (!task.description) throw new BadRequestException('Description is a required field.');
+        return this.taskService.createTask(task)
+    }
+
+    @Put(':id')
+    updateTask(@Param('id') id: number, @Body() task: TaskModel) : Observable<UpdateResult> {
+        return this.taskService.updateTask(id, task);
+    }
+
+    @Delete(':id')
+    deleteTask(@Param('id') id: number): Observable<DeleteResult> {
+        return this.taskService.deleteTask(id);
     }
 }
--- api/src/tasks/services/task.service.ts
+++ api/src/tasks/services/task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Observable, from } from 'rxjs'
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { Observable, from, map } from 'rxjs'
 import { TaskEntity } from '../models/task.entity';
 import { TaskModel } from '../models/task.interface';
 
@@ -12,11 +12,6 @@ export class TaskService {
         private readonly taskRepository: Repository<TaskEntity> 
     ) {}
 
-    //Save given task to the DB
-    createTask(task: TaskModel): Observable<TaskModel> {
-        return from(this.taskRepository.save(task)); //Convert returned value from promise to TaskModel object
-    }
-
     //Return all tasks found in DB
     getAllTasks(): Observable<TaskModel[]> { 
         return from(this.taskRepository.find()) 
@@ -25,5 +20,18 @@ export class TaskService {
     //Return specific task found in DB by title
     getTaskById(id: number): Observable<TaskModel | null> {
         return from(this.taskRepository.findOne({ where: { id } }));
+    }
+
+    //Save given task to the DB
+    createTask(task: TaskModel): Observable<TaskModel> {
+        return from(this.taskRepository.save(task)); //Convert returned value from promise to TaskModel object
+    }
+
+    updateTask(id: number, task: TaskModel): Observable<UpdateResult> { 
+        return from(this.taskRepository.update(id, task));
+    }
+
+    deleteTask(id: number): Observable<DeleteResult> {
+        return from(this.taskRepository.delete(id));
     }
 }
